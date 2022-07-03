17f6c4c378beb4627779c6abaf7928b61e492398
Benê <85972473+oibene@users.noreply.github.com>
2022-07-03 18:10:59 -0300

    Add files via upload

--- Watch_R4ts/watch_r4ts/src/actor/mouse.gd
+++ Watch_R4ts/watch_r4ts/src/actor/mouse.gd
@@ -1,28 +1,43 @@
 extends KinematicBody2D
 
 # variaveis
-export var speed: = 60 #var para mover personagem
-onready var _danca_ratinho_danca = $AnimationPlayer
-
+var velocidade: Vector2
+export(int) var speed #var para mover personagem
+onready var animacao = $animation
 
 # move nosso ratinho
-func _physics_process(delta):
-	# move personagem e faz animacao
-	if Input.is_action_pressed("move_up"):
-		move_and_slide(Vector2(0.0, -speed))
-		_danca_ratinho_danca.play("walk_up")
-		
-	elif Input.is_action_pressed("move_left"):
-		move_and_slide(Vector2(-speed, 0.0))
-		_danca_ratinho_danca.play("walk_left")
-		
-	elif Input.is_action_pressed("move_down"):
-		move_and_slide(Vector2(0.0, speed))
-		_danca_ratinho_danca.play("walk_down")
-		
-	elif Input.is_action_pressed("move_right"):
-		move_and_slide(Vector2(speed, 0.0))
-		_danca_ratinho_danca.play("walk_right")
-		
-	elif yield(get_node("AnimationPlayer"), "animation_finished"):
-	 _danca_ratinho_danca.stop()
+func _physics_process(_delta: float) -> void: #roda durante todo nosso jogo
+	mexe()
+	anima()
+	
+# move personagem
+func mexe() -> void:
+	# move ele em 8 direcoes mui pica
+	var direcao: Vector2 = Vector2 (
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	).normalized()
+	
+	velocidade = direcao * speed
+	velocidade = move_and_slide(velocidade)
+	
+# funcao para animar personagem
+func anima() -> void:
+	if velocidade != Vector2.ZERO:
+		if Input.is_action_pressed("move_right"):
+			animacao.play("walk_right")
+		if Input.is_action_pressed("move_left"):
+			animacao.play("walk_left")
+		if Input.is_action_pressed("move_down"):
+			animacao.play("walk_down")
+		if Input.is_action_pressed("move_up"):
+			animacao.play("walk_up")
+	else:
+		if Input.is_action_pressed("move_right"):
+			animacao.play("idle_right")
+		if Input.is_action_pressed("move_left"):
+			animacao.play("idle_left")
+		if Input.is_action_pressed("move_down"):
+			animacao.play("idle_down")
+		if Input.is_action_pressed("move_up"):
+			animacao.play("idle_up")
